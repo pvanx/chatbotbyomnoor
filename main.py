3073f6544be1146384026221ba2ff9f72cd00c27
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-05-30 00:24:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client 
 from nsdev import LoggerHandler, ImageGenerator, ChatbotGemini
 
-from config import BOT_TOKEN, API_ID, API_HASH, COOKIES_U COOKIES_SRCHHPGUSR GEMINI_API_KEY
+from config import BOT_TOKEN, API_ID, API_HASH, COOKIES_U, COOKIES_SRCHHPGUSR, GEMINI_API_KEY
 
 app = Client(name="genai", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
