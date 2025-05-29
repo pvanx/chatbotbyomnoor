cbfff569ee380b8c1f0cffbc5a9c638895904173
SenpaiSeeker <SenpaiSeeker@users.noreply.github.com>
2025-05-29 17:24:44 +0000

    Apply automatic changes

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from pyrogram import Client 
-from nsdev import LoggerHandler, ImageGenerator, ChatbotGemini
-
-from config import BOT_TOKEN, API_ID, API_HASH, COOKIES_U, COOKIES_SRCHHPGUSR, GEMINI_API_KEY
+from config import (
+    API_HASH,
+    API_ID,
+    BOT_TOKEN,
+    COOKIES_SRCHHPGUSR,
+    COOKIES_U,
+    GEMINI_API_KEY,
+)
+from nsdev import ChatbotGemini, ImageGenerator, LoggerHandler
+from pyrogram import Client
 
 app = Client(name="genai", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
