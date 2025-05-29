6f93f7495a16667397b43f5f7cf66fb918b40f08
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2025-05-30 00:40:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ from config import (
     COOKIES_U,
     GEMINI_API_KEY,
 )
-from nsdev import ChatbotGemini, ImageGenerator, LoggerHandler
-from pyrogram import Client
+from nsdev import ChatbotGemini, ImageGenerator, LoggerHandler, Argument 
+from pyrogram import Client, filters 
 
 app = Client(name="genai", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 chatbot = ChatbotGemini(GEMINI_API_KEY)
 image = ImageGenerator(COOKIES_U, COOKIES_SRCHHPGUSR)
 logger = LoggerHandler()
+
+
+@app.on_message(filters.command(["ai", "image", "khodam"))
+async def main_command(client, message):
+    command = message.command[0]
+    getarg = Argument().getMessage(message, is_arg=True)
+
+    if not getarg:
+        return await message.reply("**Silahkan gunakan perintah sambil masukkan yang kamu inginkan**")
+
+    if command == "ai":
+        pass
+    if command == "image":
+        pass 
+    if command == "khodam":
+        pass
